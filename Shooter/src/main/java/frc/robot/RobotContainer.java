c6fc3a26b5152a5cc186a38597ea347dc98983cf
amorygalili <amorygalili@gmail.com>
2026-01-20 18:56:58 -0500

    Added shooter subsystem

--- Shooter/src/main/java/frc/robot/RobotContainer.java
+++ Shooter/src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,7 @@ package frc.robot;
 import java.io.PrintWriter;
 
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
@@ -14,14 +15,17 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 public class RobotContainer {
 
   XboxController xboxController = new XboxController(0);
-  IntakeSubsystem intake = new IntakeSubsystem();
+  ShooterSubsystem shooter = new ShooterSubsystem();
 
   public RobotContainer() {
     configureBindings();
+
+    SmartDashboard.putData("shooterSubsystem", shooter);
+
   }
 
   private void configureBindings() {
-    new JoystickButton(xboxController, XboxController.Button.kA.value).onTrue(intake.spin()).onFalse(intake.stop());
+    new JoystickButton(xboxController, XboxController.Button.kA.value).onTrue(shooter.spin()).onFalse(shooter.stop());
 
   }
 
