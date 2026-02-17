c32f0834adf4f561fc947799777bc48dbe43d9f8
amorygalili <amorygalili@gmail.com>
2026-02-17 16:12:44 -0500

    always trench for shortest path

--- competition/src/main/java/frc/robot/subsystems/DriveShortestPath.java
+++ competition/src/main/java/frc/robot/subsystems/DriveShortestPath.java
@@ -28,14 +28,14 @@ public class DriveShortestPath {
             new Translation2d(13, 5.6), new Translation2d(13, 7.5) };
 
     // just trenches
-    // private static final Translation2d close[] = { new Translation2d(3.5, 0.5),
-    // new Translation2d(3.5, 7.5) };
-    // private static final Translation2d closeNeutral[] = { new Translation2d(6,
-    // 0.5), new Translation2d(6, 7.5) };
-    // private static final Translation2d farNeutral[] = { new Translation2d(10.5,
-    // 0.5), new Translation2d(10.5, 7.5) };
-    // private static final Translation2d far[] = { new Translation2d(13, 0.5), new
-    // Translation2d(13, 7.5) };
+    private static final Translation2d closeTrenches[] = { new Translation2d(3.5, 0.5),
+    new Translation2d(3.5, 7.5) };
+    private static final Translation2d closeNeutralTrenches[] = { new Translation2d(6,
+    0.5), new Translation2d(6, 7.5) };
+    private static final Translation2d farNeutralTrenches[] = { new Translation2d(10.5,
+    0.5), new Translation2d(10.5, 7.5) };
+    private static final Translation2d farTrenches[] = { new Translation2d(13, 0.5), new
+    Translation2d(13, 7.5) };
 
     private static final Translation2d groups[][] = { close, closeNeutral, farNeutral, far };
 
@@ -113,19 +113,29 @@ public class DriveShortestPath {
         return rotation;
     }
 
+    public Command driveShortestPath(Pose2d targetPose2d) {
+        return driveShortestPath(targetPose2d,false);
+    }
+
     /**
      * 
      * @param targetPose2d
      *                     assuming not currently on bump or in trench
      * @return
      */
-    public Command driveShortestPath(Pose2d targetPose2d) {
+    public Command driveShortestPath(Pose2d targetPose2d, boolean goesOverBumps) {
 
         Supplier<Command> someCommand = () -> {
 
             Direction pathDirection = getDirection(getPose().getTranslation(), targetPose2d.getTranslation());
             int closestGroupIndex = findIndexOfClosestGroup(groups, pathDirection);
             double handoffRadius = 0.2;
+            if (!goesOverBumps) {
+                groups[0] = closeTrenches;
+                groups[1] = closeNeutralTrenches;
+                groups[2] = farNeutralTrenches;
+                groups[3] = farTrenches;
+            }
 
             List<Path.PathElement> waypoints = new ArrayList<>();
 
