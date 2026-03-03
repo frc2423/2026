be69ffa5a8ad92381ad67a6c6021bf6054281371
tee-bee <teebeewiiu@gmail.com>
2026-03-02 19:32:49 -0500

    updateCurrentShift method

--- competition/src/main/java/frc/robot/telemetry/DashboardTelemetry.java
+++ competition/src/main/java/frc/robot/telemetry/DashboardTelemetry.java
@@ -7,12 +7,20 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color8Bit;
 import frc.robot.RobotContainer;
 
+@Logged
 public class DashboardTelemetry {
 
     private final RobotContainer robot;
 
     @Logged
-    private boolean isRedActive = false;  
+    private int currentShift = 0;
+
+
+    private String activeAlliance = null;
+    private int shiftTimeRemaining = 0; // seconds
+    private int matchTime = 160; // seconds
+
+
     private Notifier notifier = new Notifier(this::update);
 
     
@@ -24,7 +32,18 @@ public class DashboardTelemetry {
 
     }
 
-    private void update() {
-        isRedActive = HubTracker.
+    private void updateCurrentShift() {
+        String[] shiftTimes = {"30-55", "55-80", "80-105", "105-130"};
+        for (int i = 0; i < 4; i++) {
+            String[] shiftRange = shiftTimes[i].split();
+            for (int j = 0; j < shiftTimes[i].split(); j++) {
+                if (HubTracker.getMatchTime() >= shiftRange[0] && HubTracker.getMatchTime() <= shiftRange[1]) {
+                    // @Logged 
+                    currentShift = i + 1;
+                }
+            }
+        }
+
+        
     }
 }
