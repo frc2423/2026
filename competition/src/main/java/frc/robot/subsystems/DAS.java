009e721eabcb733ff27a50a30e8c3fd6321842e6
amorygalili <amorygalili@gmail.com>
2026-03-21 14:42:41 -0400

    Not finished, shoot calc on move

--- competition/src/main/java/frc/robot/subsystems/DAS.java
+++ competition/src/main/java/frc/robot/subsystems/DAS.java
@@ -1,9 +1,15 @@
 package frc.robot.subsystems;
 
 import java.util.NavigableMap;
+import java.util.Set;
 import java.util.TreeMap;
+import java.util.Map.Entry;
 
+import edu.wpi.first.math.geometry.Translation2d;
 import frc.robot.NTHelper;
+import frc.robot.commands.ShooterCommands;
+import frc.robot.generated.TunerConstants;
+import frc.robot.utils.sotm.ShotCalculator;
 
 // DAS means distance angle speed table
 public class DAS {
@@ -12,10 +18,12 @@ public class DAS {
     public class MotorSettings {
         public final double angle; // in degrees
         public final double velocity; // in volts
+        public final double tof; // time of flight
 
-        public MotorSettings(double angle, double velocity) {
+        public MotorSettings(double angle, double velocity, double tof) {
             this.angle = angle;
             this.velocity = velocity;
+            this.tof = tof;
         }
 
         public double getAngle() {
@@ -24,6 +32,10 @@ public class DAS {
 
         public double getVelocity() {
             return velocity;
+        }
+
+        public double getTof() {
+            return tof;
         }
     }
 
@@ -35,14 +47,19 @@ public class DAS {
         initializeMap();
     }
 
+    public Set<Entry<Double, MotorSettings>> getEntries() {
+        return distanceMap.entrySet();
+    }
+
     private void initializeMap() {
-        distanceMap.put(1.4, new MotorSettings(0, 2700));
-        distanceMap.put(2.0, new MotorSettings(14, 2700));
-        distanceMap.put(2.6, new MotorSettings(18.5, 2900));
-        distanceMap.put(3.2, new MotorSettings(18.5, 3100));
-        distanceMap.put(3.8, new MotorSettings(23.5, 3250));
-        distanceMap.put(4.4, new MotorSettings(27, 3450));
-        distanceMap.put(5.0, new MotorSettings(32, 3550));
+
+        distanceMap.put(1.4, new MotorSettings(0, 2700, 1));
+        distanceMap.put(2.0, new MotorSettings(14, 2700, 1));
+        distanceMap.put(2.6, new MotorSettings(18.5, 2900, 1));
+        distanceMap.put(3.2, new MotorSettings(18.5, 3100, 1));
+        distanceMap.put(3.8, new MotorSettings(23.5, 3250, 1));
+        distanceMap.put(4.4, new MotorSettings(27, 3450, 1));
+        distanceMap.put(5.0, new MotorSettings(32, 3550, 1));
 
         // distanceMap.put(1.4, new MotorSettings(5, 2900));
         // distanceMap.put(2.0, new MotorSettings(14, 2900));
@@ -82,9 +99,14 @@ public class DAS {
         double ratio = (distance - lowerKey) / (higherKey - lowerKey);
         double interpolatedAngle = lowerSettings.getAngle()
                 + ratio * (higherSettings.getAngle() - lowerSettings.getAngle());
+
         double interpolatedVoltage = lowerSettings.getVelocity()
                 + ratio * (higherSettings.getVelocity() - lowerSettings.getVelocity());
+        
+        
+        double interpolatedTof = lowerSettings.getTof()
+                + ratio * (higherSettings.getTof() - lowerSettings.getTof());
 
-        return new MotorSettings(interpolatedAngle, interpolatedVoltage);
+        return new MotorSettings(interpolatedAngle, interpolatedVoltage, interpolatedTof);
     }
 }
