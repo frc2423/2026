5d62194b6ff85b6cb019e7dba35e571392002936
amorygalili <amorygalili@gmail.com>
2026-03-23 19:28:33 -0400

    Inverted intake motor and hood, updated right apriltag camera placement

--- competition/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ competition/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -17,7 +17,7 @@ public class IntakeSubsystem extends SubsystemBase {
     private double percentSpeed = 0;
 
     public IntakeSubsystem() {
-        motorConfig.inverted(true);
+        motorConfig.inverted(false);
         setCurrentLimit(100, 100);
     }
 
